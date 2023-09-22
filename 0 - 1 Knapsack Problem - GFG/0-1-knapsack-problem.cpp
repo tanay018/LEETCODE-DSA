75c454e133f6b0f7d0a9cc2ae2ffd019f3d62c3c
Tanay Bhuta <116439205+tanay018@users.noreply.github.com>
2023-09-23 00:08:40 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,28 +8,32 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int solve(int index,int w,int wt[], int val[], vector<vector<int>>dp){
+    
+    
+    // int solve(int index,int w,int wt[], int val[], vector<vector<int>>dp){
        
-        if(index==0){
-            if(wt[0]<=w){
-                return val[0];
-            }
-            else{
-                return 0;
-            }
-        }
-        if(dp[index][w]!=-1){
-            return dp[index][w];
-        }
+    //     if(index==0){
+    //         if(wt[0]<=w){
+    //             return val[0];
+    //         }
+    //         else{
+    //             return 0;
+    //         }
+    //     }
+    //     if(dp[index][w]!=-1){
+    //         return dp[index][w];
+    //     }
         
-        int nottake=0+solve(index-1,w,wt,val,dp);
-        int take =INT_MIN;
-        if(wt[index]<=w){
-            take=val[index]+solve(index-11,w-wt[index],wt,val,dp);
-        }
-       return dp[index][w]=max(take,nottake);
+    //     int nottake=0+solve(index-1,w,wt,val,dp);
+    //     int take =INT_MIN;
+    //     if(wt[index]<=w){
+    //         take=val[index]+solve(index-11,w-wt[index],wt,val,dp);
+    //     }
+    //   return dp[index][w]=max(take,nottake);
          
-    }
+    // }
+    
+    
     int knapSack(int w, int wt[], int val[], int n) 
     { 
        // Your code here
@@ -37,22 +41,45 @@ class Solution
        //int ans=solve(n-1,w,wt,val,dp);
       // return ans;
      
-     for(int W=wt[0];W<=w;W++){
-         dp[0][W]=val[0];
+     // TABU
+    //  for(int W=wt[0];W<=w;W++){
+    //      dp[0][W]=val[0];
+    //  }
+    //  for(int index =1;index<n;index++){
+    //      for(int W=0; W<=w;W++){
+             
+    //          int nottake=0+dp[index-1][W];
+    //     int take =INT_MIN;
+    //     if(wt[index]<=W){
+    //         take=val[index]+dp[index-1][W-wt[index]];
+    //     }
+    //   dp[index][W]=max(take,nottake);
+         
+    //      }
+    //  }
+    //  return dp[n-1][w];
+    
+    vector<int>prev(w+1,0);
+    vector<int>curr(w+1,0);
+    
+      for(int W=wt[0];W<=w;W++){
+        prev[W]=val[0];
      }
      for(int index =1;index<n;index++){
          for(int W=0; W<=w;W++){
              
-             int nottake=0+dp[index-1][W];
+             int nottake=0+prev[W];
         int take =INT_MIN;
         if(wt[index]<=W){
-            take=val[index]+dp[index-1][W-wt[index]];
+            take=val[index]+prev[W-wt[index]];
         }
-       dp[index][W]=max(take,nottake);
+       curr[W]=max(take,nottake);
          
          }
+         prev = curr;
      }
-     return dp[n-1][w];
+     return prev[w];
+   
       
     }
 };
